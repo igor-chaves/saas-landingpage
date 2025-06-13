1e779c5051ee93bf4d0764b8aa23d6e0ac228b37
Igor Chaves Donega <ichaves@live.com>
2025-06-13 20:23:05 +0900

    initial boilerplate (css + folder structure)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "./components/Layout"
+import { Hero } from "./components/sections/Hero"
+
+function App() {
+  return (
+    <Layout title="Redbull">
+      <Hero />
+    </Layout>
+  )
+}
+
+export default App
